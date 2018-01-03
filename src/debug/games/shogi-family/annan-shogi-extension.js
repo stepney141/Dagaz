0c35ec84efc86e30d0bc0fd98decbb7a215e150f
GlukKazan <glukkazan@gmail.com>
2018-01-03 13:25:45 +0300

    Annan Shogi (extension)

---
+++ src/debug/games/shogi-family/annan-shogi-extension.js
@@ -0,0 +1,35 @@
+(function() {
+
+var checkVersion = Dagaz.Model.checkVersion;
+
+Dagaz.Model.checkVersion = function(design, name, value) {
+  if (name != "annan-shogi-extension") {
+      checkVersion(design, name, value);
+  }
+}
+
+var CheckInvariants = Dagaz.Model.CheckInvariants;
+
+Dagaz.Model.CheckInvariants = function(board) {
+  var design = Dagaz.Model.design;
+  var s = design.getDirection("s");
+  _.each(board.moves, function(move) {
+      if ((move.mode > 0) && (move.actions.length > 0) && (move.actions[0][0] !== null) && (move.actions[0][1] !== null)) {
+          var pos = move.actions[0][0][0];
+          var piece = board.getPiece(pos);
+          var p = design.navigate(board.player, pos, s);
+          if (p !== null) {
+              var friend = board.getPiece(p);
+              if ((friend !== null) && (friend.player == board.player)) {
+                  piece = friend;
+              }
+          }
+          if ((piece !== null) && (piece.type != move.mode)) {
+              move.failed = true;
+          }
+      }
+  });
+  CheckInvariants(board);
+}
+
+})();
