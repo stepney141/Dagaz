1b10eea3f652ad9e33d57aa02ba5416adeb47238
GlukKazan <glukkazan@gmail.com>
2018-01-24 19:12:41 +0300

    Ming Mang (board only)

--- src/debug/games/custodian/ming-mang.js
+++ src/debug/games/custodian/ming-mang.js
@@ -31,8 +31,8 @@ Dagaz.Model.BuildDesign = function(design) {
     design.addDirection("s");
     design.addDirection("n");
 
-    design.addPlayer("White", [1, 0, 3, 2]);
-    design.addPlayer("Black", [0, 1, 2, 3]);
+    design.addPlayer("Black", [1, 0, 3, 2]);
+    design.addPlayer("White", [0, 1, 2, 3]);
 
     design.addPosition("a17", [0, 1, 17, 0]);
     design.addPosition("b17", [-1, 1, 17, 0]);
