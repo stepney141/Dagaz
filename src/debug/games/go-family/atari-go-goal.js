2768c36e387d2cea2821c54590d3724048705a9b
GlukKazan <glukkazan@gmail.com>
2018-04-01 15:36:41 +0200

    show-drops=all

---
+++ src/debug/games/go-family/atari-go-goal.js
@@ -0,0 +1,29 @@
+(function() {
+
+var checkVersion = Dagaz.Model.checkVersion;
+
+Dagaz.Model.checkVersion = function(design, name, value) {
+  if (name != "atari-go-goal") {
+      checkVersion(design, name, value);
+  }
+}
+
+var checkGoals = Dagaz.Model.checkGoals;
+
+Dagaz.Model.checkGoals = function(design, board, player) {
+  if (!_.isUndefined(board.move)) {
+      var captures = _.filter(board.move.actions, function(a) {
+          return (a[0] !== null) && (a[1] === null);
+      });
+      if (captures.length > 0) {
+          if (board.player == player){
+              return -1;
+          } else {
+              return 1;
+          }
+      }
+  }
+  return checkGoals(design, board, player);
+}
+
+})();
