544b4536dd90c232fb58ac7ee6c22c76a3cbbfa5
GlukKazan <glukkazan@gmail.com>
2018-04-26 16:45:45 +0200

    Morris (prototype)

--- src/debug/games/morris/morris-extension.js
+++ src/debug/games/morris/morris-extension.js
@@ -1,164 +1,68 @@
 (function() {
 
 var checkVersion = Dagaz.Model.checkVersion;
-var multiMode = false;
-var koMode    = false;
 
 Dagaz.Model.checkVersion = function(design, name, value) {
-  if (name == "morris-extension") {
-      if (value == "multi") {
-          multiMode = true;
-      }
-      if (value == "ko") {
-          koMode = true;
-      }
-  } else {
-      checkVersion(design, name, value);
+  if (name != "morris-extension") {
+     checkVersion(design, name, value);
   }
 }
 
-var getEmptyPositions = function(board) {
-  return _.chain(_.range(Dagaz.Model.design.positions.length))
-   .filter(function(pos) {
-       return (board.getPiece(pos) === null);
-    })
-   .value();
+var isLine = function(design, board, player, pos, dir, empty) {
+  var p = design.navigate(player, pos, dir);
+  if (p === null) return false;
+  if ((empty !== null) && (p == empty)) return false;
+  var piece = board.getPiece(p);
+  if (piece === null) return false;
+  if (piece.player != player) return false;
+  p = design.navigate(player, p, dir);
+  if ((empty !== null) && (p == empty)) return false;
+  piece = board.getPiece(p);
+  if (piece === null) return false;
+  return piece.player == player;
 }
 
-var getEnemyPositions = function(board) {
-  return _.chain(_.range(Dagaz.Model.design.positions.length))
-   .filter(function(pos) {
-       var piece = board.getPiece(pos);
-       if (piece === null) return false;
-       return (piece.player != board.player);
-    })
-   .push(null)
-   .value();
-}
-
-var getFriendPositions = function(board) {
-  return _.chain(_.range(Dagaz.Model.design.positions.length))
-   .filter(function(pos) {
-       var piece = board.getPiece(pos);
-       if (piece === null) return false;
-       return (piece.player === board.player);
-    })
-   .value();
-}
-
-var isFriend = function(board, pos, player) {
-   if (pos === null) return false;
-   var piece = board.getPiece(pos);
-   if (piece === null) return false;
-   return (piece.player == player);
-}
-
-var countLines = function(board, pos, player) {
-  var r = 0;
-  var design = Dagaz.Model.design;
-  _.chain(_.range(design.dirs.length))
-   .each(function(dir) {
-       var p = design.navigate(player, pos, dir);
-       if (isFriend(board, p, player)) {
-           p = design.navigate(player, p, dir);
-           if (isFriend(board, p, player)) r++;
-           p = design.navigate(0, pos, dir);
-           if (isFriend(board, p, player)) r++;
-       }
-    });
-  return r;
-}
-
-var createResultMoves = function(board, src, dests) {
-  dests = _.filter(function(p) {
-      var m = Dagaz.Model.createMove();
-      m.movePiece(src, p, null, 1);
-      var b = board.apply(m);
-      return countLines(b, p, board.player) > 0;
-  });
-  if (dests.length > 0) {
-      var move = Dagaz.Model.createMove();
-      move.actions.push([ [src], dests, null, 1]);
-      move.actions.push([ getEnemyPositions(board), null, null, 1]);
-      board.moves.push(move);
-  }
-}
-
-var createNoResultMoves = function(board, src, dests) {
-  dests = _.filter(function(p) {
-      var m = Dagaz.Model.createMove();
-      m.movePiece(src, p, null, 1);
-      var b = board.apply(m);
-      return countLines(b, p, board.player) == 0;
-  });
-  if (dests.length > 0) {
-      var move = Dagaz.Model.createMove();
-      move.actions.push([ [src], dests, null, 1]);
-      board.moves.push(move);
-  }
-}
-
-var isMovePhase = function(board) {
-  if (board.moves.length == 0) return false;
-  if (board.moves[0].actions.length == 0) return false;
-  var fp = board.moves[0].actions[0][0];
-  var tp = board.moves[0].actions[0][1];
-  return (fp !== null) && (tp !== null);
+var isMiddle = function(design, board, player, pos, dir, empty) {
+  var p = design.navigate(player, pos, dir);
+  if (p === null) return false;
+  if ((empty !== null) && (p == empty)) return false;
+  var piece = board.getPiece(p);
+  if (piece === null) return false;
+  if (piece.player != player) return false;
+  p = design.navigate(0, pos, dir);
+  if ((empty !== null) && (p == empty)) return false;
+  piece = board.getPiece(p);
+  if (piece === null) return false;
+  return piece.player == player;
 }
 
 var CheckInvariants = Dagaz.Model.CheckInvariants;
 
 Dagaz.Model.CheckInvariants = function(board) {
   var design = Dagaz.Model.design;
-  var friends = getFriendPositions(board);
-  if (isMovePhase(board) && (friends.length == 3)) {
-      board.moves = [];
-      _.each(friends, function(pos) {
-          var e = getEmptyPositions(board);
-          createResultMoves(board, pos, e);
-          createNoResultMoves(board, pos, e);
-      });
-      CheckInvariants(board);
-      return;
-  }
-  _.each(board.moves, function(m) {
-      var b = board.apply(m);
-      _.chain(m.actions)
-       .filter(function(action) {
-           return (action[1] !== null);
-        })
-       .first();
-       .each(function(action) {
-           fp = action[0];
-           tp = action[1];
-           pn = action[3];
-           var cnt = countLines(b, tp[0], board.player);
-           if (cnt > 0) {
-               if (koMode && (fp !== null)) {
-                   if (countLines(board, fp[0], board.player) > 0) {
-                       m.failed = true;
-                   }
-               }
-               if (!multiMode) {
-                   cnt = 1;
-               }
-               var all = getEnemyPositions(board);
-               var captured = _.filter(all, function(pos) {
-                   if (pos === null) return true;
-                   var piece = board.getPiece(pos);
-                   if (piece === null) return false;
-                   return countLines(board, pos, piece.player) == 0;
-               });
-               if (captured.length == 0) {
-                   captured = all;
-               }
-               if (captured.length > 0) {
-                   for (k = 0; k < cnt; k++) {
-                        m.actions.push([captured, null, null, pn]);
-                   }
-               }
-           }
-        });
+  var dirs   = [];
+  dirs.push(design.getDirection("n")); dirs.push(design.getDirection("ne"));
+  dirs.push(design.getDirection("e")); dirs.push(design.getDirection("se"));
+  _.each(board.moves, function(move) {
+      var cnt   = 0;
+      var empty = null;
+      if ((move.actions.length > 0) && (move.actions[0][1] !== null)) {
+          var pos = move.actions[0][1][0];
+          if (move.actions[0][0] !== null) {
+              empty = move.actions[0][0][0];
+          }
+          _.each(design.allDirections(), function(dir) {
+              if (isLine(design, board, board.player, pos, dir, empty)) {
+                  cnt++;
+              }
+          });
+          _.each(dirs, function(dir) {
+              if (isMiddle(design, board, board.player, pos, dir, empty)) {
+                  cnt++;
+              }
+          });
+      }
+      move.mode = cnt;
   });
   CheckInvariants(board);
 }
