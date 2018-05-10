253693d52b0174b6bdf8674501d5d1c10aa054df
GlukKazan <glukkazan@gmail.com>
2018-05-10 17:23:30 +0200

    Bagh-Chal (completed)

--- src/debug/games/checkers-like/bagh-chal-extension.js
+++ src/debug/games/checkers-like/bagh-chal-extension.js
@@ -8,6 +8,28 @@ Dagaz.Model.checkVersion = function(design, name, value) {
   }
 }
 
+Dagaz.AI.heuristic = function(ai, design, board, move) {
+  var r = 0;
+  if (move.actions.length > 1) {
+      return 100 * move.actions.length;
+  }
+  if (move.isSimpleMove()) {
+      var pos = move.actions[0][1][0];
+      _.each(design.allDirections(), function(dir) {
+          var p = design.navigate(board.player, pos, dir);
+          if (p === null) return;
+          var piece = board.getPiece(p);
+          if (piece === null) return;
+          if (piece.player == board.player) return;
+          p = design.navigate(board.player, p, dir);
+          if (p === null) return;
+          if (board.getPiece(p) !== null) return;
+          r += 10;
+      });
+  }
+  return r;
+}
+
 var CheckInvariants = Dagaz.Model.CheckInvariants;
 
 Dagaz.Model.CheckInvariants = function(board) {
