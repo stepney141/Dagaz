e24c878ee58ff6f3d9d9d2b8897b8fe0f16d7e20
GlukKazan <glukkazan@gmail.com>
2018-05-18 18:43:52 +0200

    Yote (fix)

--- src/debug/games/checkers-like/yote-extension.js
+++ src/debug/games/checkers-like/yote-extension.js
@@ -8,8 +8,94 @@ Dagaz.Model.checkVersion = function(design, name, value) {
   }
 }
 
+var isDanger = function(design, board, pos, dir, empty) {
+  var p = design.navigate(board.player, pos, dir);
+  if (p === null) return false;
+  var piece = board.getPiece(p);
+  if ((piece === null) || (piece.player == board.player)) return false;
+  p = design.navigate(0, pos, dir);
+  if (p === null) return false;
+  if (!_.isUndefined(empty) && (p == empty)) return true;
+  return board.getPiece(p) === null;
+}
+
+var isTarget = function(design, board, pos, dir) {
+  var p = design.navigate(board.player, pos, dir);
+  if (p === null) return false;
+  var piece = board.getPiece(p);
+  if ((piece === null) || (piece.player == board.player)) return false;
+  p = design.navigate(board.player, p, dir);
+  if (p === null) return false;
+  return board.getPiece(p) === null;
+}
+
+var isHelp = function(design, board, pos, dir, empty) {
+  var p = design.navigate(board.player, pos, dir);
+  if (p === null) return false;
+  if (!_.isUndefined(empty) && (p == empty)) return false;
+  var piece = board.getPiece(p);
+  if ((piece === null) || (piece.player != board.player)) return false;
+  p = design.navigate(board.player, p, dir);
+  if (p === null) return false;
+  piece = board.getPiece(p);
+  if (piece === null) return false;
+  return piece.player != board.player;
+}
+
+var isSafe = function(design, board, pos, dir) {
+  var p = design.navigate(board.player, pos, dir);
+  if (p === null) return false;
+  var piece = board.getPiece(p);
+  if ((piece === null) || (piece.player != board.player)) return false;
+  p = design.navigate(board.player, p, dir);
+  if (p === null) return false;
+  return board.getPiece(p) === null;
+}
+
+var isEnemy = function(design, board, pos, dir) {
+  var p = design.navigate(board.player, pos, dir);
+  if (p === null) return false;
+  var piece = board.getPiece(p);
+  if ((piece === null) || (piece.player == board.player)) return false;
+  p = design.navigate(board.player, p, dir);
+  if (p === null) return false;
+  piece = board.getPiece(p);
+  if (piece === null) return false;
+  return piece.player == board.player;
+}
+
 Dagaz.AI.heuristic = function(ai, design, board, move) {
-  return move.actions.length;
+  var r = 0;
+  _.each(move.actions, function(a) {
+      if ((a[0] !== null) && (a[1] === null)) {
+          var v = 10;
+          var pos = a[0][0];
+          for (var dir = 0; dir < design.dirs.length; dir++) {
+              if (isHelp(design, board, pos, dir)) return -1;
+              if (isSafe(design, board, pos, dir)) r += 10;
+              if (isEnemy(design, board, pos, dir)) r += 5;
+          }
+          r += v;
+      }
+      if ((a[0] === null) && (a[1] !== null)) {
+          var pos = a[1][0];
+          for (var dir = 0; dir < design.dirs.length; dir++) {
+              if (isDanger(design, board, pos, dir)) return -1;
+              if (isTarget(design, board, pos, dir)) r += 10;
+              if (isHelp(design, board, pos, dir)) r += 5;
+          }
+      }
+  });
+  if ((move.actions.length == 1) && (move.actions[0][0] !== null) && (move.actions[0][1] !== null)) {
+      var empty = move.actions[0][0][0];
+      var pos   = move.actions[0][1][0];
+      for (var dir = 0; dir < design.dirs.length; dir++) {
+          if (isDanger(design, board, pos, dir, empty)) return -1;
+          if (isTarget(design, board, pos, dir)) r += 10;
+          if (isHelp(design, board, pos, dir, empty)) r += 5;
+      }
+  }
+  return r;
 }
 
 var isCapture = function(move) {
