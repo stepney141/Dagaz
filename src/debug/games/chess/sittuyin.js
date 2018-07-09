880fc1438f3e47ef714534b532ebbf30d811964c
GlukKazan <glukkazan@gmail.com>
2018-07-09 16:48:09 +0200

    Genesis Chess (prototype)

--- src/debug/games/chess/sittuyin.js
+++ src/debug/games/chess/sittuyin.js
@@ -21,12 +21,16 @@ ZRF = {
 
 Dagaz.Model.BuildDesign = function(design) {
     design.checkVersion("z2j", "2");
+    design.checkVersion("animate-drops", "false");
     design.checkVersion("animate-captures", "false");
     design.checkVersion("smart-moves", "false");
     design.checkVersion("show-blink", "false");
-    design.checkVersion("sittuyin-invariant", "true");
+    design.checkVersion("show-hints", "false");
+    design.checkVersion("show-drops", "true");
+    design.checkVersion("show-captures", "false");
+//  design.checkVersion("sittuyin-invariant", "true");
     design.checkVersion("sittuyin-promotion", "true");
-    design.checkVersion("sittuyin-setup", "true");
+//  design.checkVersion("sittuyin-setup", "true");
 
     design.addDirection("w");
     design.addDirection("e");
@@ -39,6 +43,17 @@ Dagaz.Model.BuildDesign = function(design) {
 
     design.addPlayer("White", [1, 0, 4, 6, 2, 7, 3, 5]);
     design.addPlayer("Black", [0, 1, 4, 5, 2, 3, 7, 6]);
+    design.addTurn(1);
+    design.addTurn(1);
+    design.addTurn(1);
+    design.addTurn(1);
+    design.addTurn(1);
+    design.addTurn(1);
+    design.addTurn(1);
+    design.addTurn(1);
+    design.repeatMark();
+    design.addTurn(1);
+    design.addTurn(2);
 
     design.addPosition("a8", [0, 1, 8, 0, 0, 9, 0, 0]);
     design.addPosition("b8", [-1, 1, 8, 0, 0, 9, 7, 0]);
@@ -112,11 +127,9 @@ Dagaz.Model.BuildDesign = function(design) {
     design.addZone("second-rank", 1, [48, 49, 50, 51, 52, 53, 54, 55, 44, 45, 46, 47]);
     design.addZone("second-rank", 2, [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
 
-    design.addCommand(0, ZRF.FUNCTION,	24);	// from
-    design.addCommand(0, ZRF.PARAM,	0);	// $1
-    design.addCommand(0, ZRF.FUNCTION,	22);	// navigate
-    design.addCommand(0, ZRF.FUNCTION,	3);	// friend?
-    design.addCommand(0, ZRF.FUNCTION,	0);	// not
+    design.addCommand(0, ZRF.IN_ZONE,	2);	// second-rank
+    design.addCommand(0, ZRF.FUNCTION,	20);	// verify
+    design.addCommand(0, ZRF.FUNCTION,	1);	// empty?
     design.addCommand(0, ZRF.FUNCTION,	20);	// verify
     design.addCommand(0, ZRF.FUNCTION,	25);	// to
     design.addCommand(0, ZRF.FUNCTION,	28);	// end
@@ -124,28 +137,15 @@ Dagaz.Model.BuildDesign = function(design) {
     design.addCommand(1, ZRF.FUNCTION,	24);	// from
     design.addCommand(1, ZRF.PARAM,	0);	// $1
     design.addCommand(1, ZRF.FUNCTION,	22);	// navigate
-    design.addCommand(1, ZRF.FUNCTION,	1);	// empty?
-    design.addCommand(1, ZRF.FUNCTION,	0);	// not
-    design.addCommand(1, ZRF.IF,	7);
-    design.addCommand(1, ZRF.FORK,	3);
-    design.addCommand(1, ZRF.FUNCTION,	25);	// to
-    design.addCommand(1, ZRF.FUNCTION,	28);	// end
-    design.addCommand(1, ZRF.PARAM,	1);	// $2
-    design.addCommand(1, ZRF.FUNCTION,	22);	// navigate
-    design.addCommand(1, ZRF.JUMP,	-8);
     design.addCommand(1, ZRF.FUNCTION,	3);	// friend?
     design.addCommand(1, ZRF.FUNCTION,	0);	// not
     design.addCommand(1, ZRF.FUNCTION,	20);	// verify
     design.addCommand(1, ZRF.FUNCTION,	25);	// to
     design.addCommand(1, ZRF.FUNCTION,	28);	// end
 
-    design.addCommand(2, ZRF.FUNCTION,	24);	// from
-    design.addCommand(2, ZRF.PARAM,	0);	// $1
-    design.addCommand(2, ZRF.FUNCTION,	22);	// navigate
-    design.addCommand(2, ZRF.PARAM,	1);	// $2
-    design.addCommand(2, ZRF.FUNCTION,	22);	// navigate
-    design.addCommand(2, ZRF.FUNCTION,	3);	// friend?
-    design.addCommand(2, ZRF.FUNCTION,	0);	// not
+    design.addCommand(2, ZRF.IN_ZONE,	1);	// first-rank
+    design.addCommand(2, ZRF.FUNCTION,	20);	// verify
+    design.addCommand(2, ZRF.FUNCTION,	1);	// empty?
     design.addCommand(2, ZRF.FUNCTION,	20);	// verify
     design.addCommand(2, ZRF.FUNCTION,	25);	// to
     design.addCommand(2, ZRF.FUNCTION,	28);	// end
@@ -154,81 +154,119 @@ Dagaz.Model.BuildDesign = function(design) {
     design.addCommand(3, ZRF.PARAM,	0);	// $1
     design.addCommand(3, ZRF.FUNCTION,	22);	// navigate
     design.addCommand(3, ZRF.FUNCTION,	1);	// empty?
-    design.addCommand(3, ZRF.FUNCTION,	20);	// verify
-    design.addCommand(3, ZRF.IN_ZONE,	0);	// promotion
     design.addCommand(3, ZRF.FUNCTION,	0);	// not
-    design.addCommand(3, ZRF.IF,	4);
-    design.addCommand(3, ZRF.PROMOTE,	1);	// Sit-ke
+    design.addCommand(3, ZRF.IF,	7);
+    design.addCommand(3, ZRF.FORK,	3);
     design.addCommand(3, ZRF.FUNCTION,	25);	// to
-    design.addCommand(3, ZRF.JUMP,	2);
+    design.addCommand(3, ZRF.FUNCTION,	28);	// end
+    design.addCommand(3, ZRF.PARAM,	1);	// $2
+    design.addCommand(3, ZRF.FUNCTION,	22);	// navigate
+    design.addCommand(3, ZRF.JUMP,	-8);
+    design.addCommand(3, ZRF.FUNCTION,	3);	// friend?
+    design.addCommand(3, ZRF.FUNCTION,	0);	// not
+    design.addCommand(3, ZRF.FUNCTION,	20);	// verify
     design.addCommand(3, ZRF.FUNCTION,	25);	// to
     design.addCommand(3, ZRF.FUNCTION,	28);	// end
 
     design.addCommand(4, ZRF.FUNCTION,	24);	// from
     design.addCommand(4, ZRF.PARAM,	0);	// $1
     design.addCommand(4, ZRF.FUNCTION,	22);	// navigate
-    design.addCommand(4, ZRF.FUNCTION,	2);	// enemy?
-    design.addCommand(4, ZRF.FUNCTION,	20);	// verify
-    design.addCommand(4, ZRF.IN_ZONE,	0);	// promotion
+    design.addCommand(4, ZRF.PARAM,	1);	// $2
+    design.addCommand(4, ZRF.FUNCTION,	22);	// navigate
+    design.addCommand(4, ZRF.FUNCTION,	3);	// friend?
     design.addCommand(4, ZRF.FUNCTION,	0);	// not
-    design.addCommand(4, ZRF.IF,	4);
-    design.addCommand(4, ZRF.PROMOTE,	1);	// Sit-ke
-    design.addCommand(4, ZRF.FUNCTION,	25);	// to
-    design.addCommand(4, ZRF.JUMP,	2);
+    design.addCommand(4, ZRF.FUNCTION,	20);	// verify
     design.addCommand(4, ZRF.FUNCTION,	25);	// to
     design.addCommand(4, ZRF.FUNCTION,	28);	// end
 
     design.addCommand(5, ZRF.FUNCTION,	24);	// from
-    design.addCommand(5, ZRF.IN_ZONE,	0);	// promotion
+    design.addCommand(5, ZRF.PARAM,	0);	// $1
+    design.addCommand(5, ZRF.FUNCTION,	22);	// navigate
+    design.addCommand(5, ZRF.FUNCTION,	1);	// empty?
     design.addCommand(5, ZRF.FUNCTION,	20);	// verify
+    design.addCommand(5, ZRF.IN_ZONE,	0);	// promotion
+    design.addCommand(5, ZRF.FUNCTION,	0);	// not
+    design.addCommand(5, ZRF.IF,	4);
     design.addCommand(5, ZRF.PROMOTE,	1);	// Sit-ke
+    design.addCommand(5, ZRF.FUNCTION,	25);	// to
+    design.addCommand(5, ZRF.JUMP,	2);
     design.addCommand(5, ZRF.FUNCTION,	25);	// to
     design.addCommand(5, ZRF.FUNCTION,	28);	// end
 
-    design.addPiece("Min-gyi", 0, 1000);
-    design.addMove(0, 0, [4], 0);
-    design.addMove(0, 0, [2], 0);
-    design.addMove(0, 0, [0], 0);
-    design.addMove(0, 0, [1], 0);
-    design.addMove(0, 0, [7], 0);
-    design.addMove(0, 0, [6], 0);
-    design.addMove(0, 0, [3], 0);
-    design.addMove(0, 0, [5], 0);
+    design.addCommand(6, ZRF.FUNCTION,	24);	// from
+    design.addCommand(6, ZRF.PARAM,	0);	// $1
+    design.addCommand(6, ZRF.FUNCTION,	22);	// navigate
+    design.addCommand(6, ZRF.FUNCTION,	2);	// enemy?
+    design.addCommand(6, ZRF.FUNCTION,	20);	// verify
+    design.addCommand(6, ZRF.IN_ZONE,	0);	// promotion
+    design.addCommand(6, ZRF.FUNCTION,	0);	// not
+    design.addCommand(6, ZRF.IF,	4);
+    design.addCommand(6, ZRF.PROMOTE,	1);	// Sit-ke
+    design.addCommand(6, ZRF.FUNCTION,	25);	// to
+    design.addCommand(6, ZRF.JUMP,	2);
+    design.addCommand(6, ZRF.FUNCTION,	25);	// to
+    design.addCommand(6, ZRF.FUNCTION,	28);	// end
 
-    design.addPiece("Sit-ke", 1, 4);
-    design.addMove(1, 0, [7], 0);
-    design.addMove(1, 0, [6], 0);
-    design.addMove(1, 0, [3], 0);
-    design.addMove(1, 0, [5], 0);
+    design.addCommand(7, ZRF.FUNCTION,	24);	// from
+    design.addCommand(7, ZRF.IN_ZONE,	0);	// promotion
+    design.addCommand(7, ZRF.FUNCTION,	20);	// verify
+    design.addCommand(7, ZRF.PROMOTE,	1);	// Sit-ke
+    design.addCommand(7, ZRF.FUNCTION,	25);	// to
+    design.addCommand(7, ZRF.FUNCTION,	28);	// end
 
-    design.addPiece("Yahhta", 2, 20);
-    design.addMove(2, 1, [4, 4], 0);
-    design.addMove(2, 1, [2, 2], 0);
-    design.addMove(2, 1, [0, 0], 0);
-    design.addMove(2, 1, [1, 1], 0);
+    design.addPriority(0);			// priority-type
+    design.addPriority(1);			// drop-type
+    design.addPriority(2);			// normal-type
 
-    design.addPiece("Sin", 3, 5);
-    design.addMove(3, 0, [7], 0);
-    design.addMove(3, 0, [6], 0);
-    design.addMove(3, 0, [3], 0);
-    design.addMove(3, 0, [5], 0);
-    design.addMove(3, 0, [4], 0);
+    design.addPiece("Min-gyi", 0);
+    design.addDrop(0, 0, [], 0);
+    design.addMove(0, 1, [4], 2);
+    design.addMove(0, 1, [2], 2);
+    design.addMove(0, 1, [0], 2);
+    design.addMove(0, 1, [1], 2);
+    design.addMove(0, 1, [7], 2);
+    design.addMove(0, 1, [6], 2);
+    design.addMove(0, 1, [3], 2);
+    design.addMove(0, 1, [5], 2);
 
-    design.addPiece("Myin", 4, 8);
-    design.addMove(4, 2, [4, 7], 0);
-    design.addMove(4, 2, [4, 3], 0);
-    design.addMove(4, 2, [2, 6], 0);
-    design.addMove(4, 2, [2, 5], 0);
-    design.addMove(4, 2, [0, 7], 0);
-    design.addMove(4, 2, [0, 6], 0);
-    design.addMove(4, 2, [1, 3], 0);
-    design.addMove(4, 2, [1, 5], 0);
+    design.addPiece("Sit-ke", 1);
+    design.addDrop(1, 0, [], 1);
+    design.addMove(1, 1, [7], 2);
+    design.addMove(1, 1, [6], 2);
+    design.addMove(1, 1, [3], 2);
+    design.addMove(1, 1, [5], 2);
 
-    design.addPiece("Ne", 5, 2);
-    design.addMove(5, 3, [4], 0);
-    design.addMove(5, 4, [7], 0);
-    design.addMove(5, 4, [3], 0);
-    design.addMove(5, 5, [], 0);
+    design.addPiece("Yahhta", 2);
+    design.addDrop(2, 2, [], 1);
+    design.addMove(2, 3, [4, 4], 2);
+    design.addMove(2, 3, [2, 2], 2);
+    design.addMove(2, 3, [0, 0], 2);
+    design.addMove(2, 3, [1, 1], 2);
+
+    design.addPiece("Sin", 3);
+    design.addDrop(3, 0, [], 1);
+    design.addMove(3, 1, [7], 2);
+    design.addMove(3, 1, [6], 2);
+    design.addMove(3, 1, [3], 2);
+    design.addMove(3, 1, [5], 2);
+    design.addMove(3, 1, [4], 2);
+
+    design.addPiece("Myin", 4);
+    design.addDrop(4, 0, [], 1);
+    design.addMove(4, 4, [4, 7], 2);
+    design.addMove(4, 4, [4, 3], 2);
+    design.addMove(4, 4, [2, 6], 2);
+    design.addMove(4, 4, [2, 5], 2);
+    design.addMove(4, 4, [0, 7], 2);
+    design.addMove(4, 4, [0, 6], 2);
+    design.addMove(4, 4, [1, 3], 2);
+    design.addMove(4, 4, [1, 5], 2);
+
+    design.addPiece("Ne", 5);
+    design.addMove(5, 5, [4], 2);
+    design.addMove(5, 6, [7], 2);
+    design.addMove(5, 6, [3], 2);
+    design.addMove(5, 7, [], 2);
 
     design.setup("White", "Ne", 40);
     design.setup("White", "Ne", 41);
@@ -251,11 +289,6 @@ Dagaz.Model.BuildDesign = function(design) {
     design.setup("Black", "Ne", 21);
     design.setup("Black", "Ne", 22);
     design.setup("Black", "Ne", 23);
-    design.reserve("Black", "Min-gyi", 1);
-    design.reserve("Black", "Sit-ke", 1);
-    design.reserve("Black", "Yahhta", 2);
-    design.reserve("Black", "Myin", 2);
-    design.reserve("Black", "Sin", 2);
 }
 
 Dagaz.View.configure = function(view) {
