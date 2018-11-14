b65a5f7c2b78deefac94677c8ab9d07919b52057
GlukKazan <glukkazan@gmail.com>
2018-11-14 15:25:40 +0300

    Renju (fix)

--- src/debug/games/races/halma-8x8.js
+++ src/debug/games/races/halma-8x8.js
@@ -27,7 +27,7 @@ Dagaz.Model.BuildDesign = function(design) {
     design.checkVersion("smart-moves", "false");
     design.checkVersion("pass-partial", "true");
     design.checkVersion("detect-loops", "true");
-    design.checkVersion("advisor-wait", "5");
+    design.checkVersion("advisor-wait", "1");
     design.checkVersion("halma-restrictions", "strong");
 
     design.addDirection("se"); // 0
