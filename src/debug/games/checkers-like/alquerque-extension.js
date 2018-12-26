2645373f6fb50985cc521a92f7912617e4318dc3
GlukKazan <glukkazan@gmail.com>
2018-12-26 17:20:35 +0300

    Alquerque (forced AI)

--- src/debug/games/checkers-like/alquerque-extension.js
+++ src/debug/games/checkers-like/alquerque-extension.js
@@ -1,5 +1,10 @@
 (function() {
 
+Dagaz.AI.AI_FRAME      = 2000;
+Dagaz.AI.MAX_DEEP      = 3;
+
+var MAX_FORCED_FACTOR  = 2;
+
 var strictMode = false;
 
 var checkVersion = Dagaz.Model.checkVersion;
@@ -10,60 +15,68 @@ Dagaz.Model.checkVersion = function(design, name, value) {
   }
 }
 
-var isAttacked = function(design, board, pos, empty, dir, opposite) {
-  var p = design.navigate(board.player, pos, dir);
-  if ((p === null) || (p == empty)) return false;
-  var piece = board.getPiece(p);
-  if ((p === null) || (p.player == board.player)) return false;
-  p = design.navigate(board.player, pos, opposite);
-  if (p === null) return false;
-  return (p == empty) || (board.getPiece(p) === null);
+Dagaz.AI.heuristic = function(ai, design, board, move) {
+  var r = 1;
+  _.each(move.actions, function(a) {
+      if ((a[0] !== null) && (a[1] === null)) {
+           var piece = board.getPiece(a[0][0]);
+           if (piece !== null) {
+               r += design.price[piece.type];
+           }
+      }
+  });
+  return r;
 }
 
-Dagaz.AI.heuristic = function(ai, design, board, move) {
-  if ((move.actions.length == 1) && (move.actions[0][0] !== null) && (move.actions[0][1] !== null)) {
-      var n = design.getDirection("n"); var nw = design.getDirection("nw"); 
-      var s = design.getDirection("s"); var sw = design.getDirection("sw");
-      var w = design.getDirection("w"); var ne = design.getDirection("ne"); 
-      var e = design.getDirection("e"); var se = design.getDirection("se");
-      var sr = move.actions[0][0][0];   var ds = move.actions[0][1][0];
-      if (isAttacked(design, board, ds, sr, nw, se) ||
-          isAttacked(design, board, ds, sr, ne, sw) ||
-          isAttacked(design, board, ds, sr, se, nw) ||
-          isAttacked(design, board, ds, sr, sw, ne) ||
-          isAttacked(design, board, ds, sr,  n,  s) ||
-          isAttacked(design, board, ds, sr,  s,  n) ||
-          isAttacked(design, board, ds, sr,  w,  e) ||
-          isAttacked(design, board, ds, sr,  e,  w)) {
-          return 3;
+Dagaz.AI.isForced = function(design, board, move) {
+  if (_.isUndefined(move.isForced)) {
+      move.isForced = false;
+      var b = board.apply(move);
+      var c = 0;
+      _.each(design.allPositions(), function(pos) {
+          var piece = b.getPiece(pos);
+          if ((piece !== null) && (piece.player == b.player)) {
+              _.each(design.allDirections(), function(dir) {
+                   var p = design.navigate(b.player, pos, dir);
+                   if (p !== null) {
+                       piece = b.getPiece(p);
+                       if ((piece !== null) && (piece.player != b.player)) {
+                            p = design.navigate(b.player, p, dir);
+                            if ((p !== null) && (b.getPiece(p) === null)) c++;
+                       }
+                   }
+              });
+          }
+      });
+      if ((c > 0) && (c <= MAX_FORCED_FACTOR)) {
+          move.isForced = true;
       }
   }
-  return 1;
+  return move.isForced;
+}
+
+Dagaz.AI.getEval = function(design, board) {
+  if (_.isUndefined(board.eval)) {
+      board.eval = 0;
+      _.each(design.allPositions(), function(pos) {
+          var piece = board.getPiece(pos);
+          if (piece !== null) {
+              var v = design.price[piece.type];
+              if (!Dagaz.AI.isFriend(board.player, piece.player)) {
+                  v = -v;
+              }
+              board.eval += v;
+          }
+      });
+  }
+  return board.eval;
 }
 
 Dagaz.AI.eval = function(design, params, board, player) {
-  var r = 0;
-  _.each(design.allPositions(), function(pos) {
-      var piece = board.getPiece(pos);
-      if (piece !== null) {
-          var v = design.price[piece.type];
-          var bonus = 8;
-          if (_.indexOf([20, 0, 24, 4, 15, 5, 21, 1, 23, 3, 19, 9], +pos) >= 0) {
-              bonus -= 5;
-          }
-          if (_.indexOf([11, 17, 7, 13], +pos) >= 0) {
-              bonus -= 4;
-          }
-          if (_.indexOf([10, 22, 2, 14], +pos) >= 0) {
-              bonus -= 3;
-          }
-          v += bonus;
-          if (!Dagaz.AI.isFriend(player, piece.player)) {
-              v = -v;
-          }
-          r += v;
-      }
-  });
+  var r = Dagaz.AI.getEval(design, board);
+  if (!Dagaz.AI.isFriend(player, board.player)) {
+      r = -r;
+  }
   return r;
 }
 
