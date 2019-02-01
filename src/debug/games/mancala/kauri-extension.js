a8643b7754b46aa54539f4b317efc3de3d0ca406
GlukKazan <glukkazan@gmail.com>
2019-02-01 16:28:46 +0300

    Kauri (prototype)

--- src/debug/games/mancala/kauri-extension.js
+++ src/debug/games/mancala/kauri-extension.js
@@ -8,58 +8,112 @@ Dagaz.Model.checkVersion = function(design, name, value) {
   }
 }
 
-var isKauri = function(pieces) {
-  for (var i in pieces) {
-       if (pieces[i].toString() == "Kauri") {
-           return true;
-       }
+var toReserve = function(design, board, player, move, cnt) {
+  var pos = design.navigate(player, 0, 2);
+  if ((cnt > 0) && (pos !== null)) {
+      piece = board.getPiece(pos);
+      if (piece === null) {
+          piece = Dagaz.Model.createPiece(0, player);
+          piece = piece.setValue(0, cnt).setValue(1, 0);
+          move.dropPiece(pos, piece);
+      } else {
+          cnt += +piece.getValue(0);
+          piece = piece.setValue(0, cnt).setValue(1, 0);
+          move.movePiece(pos, pos, piece);
+      }
   }
-  return false;
 }
 
 var CheckInvariants = Dagaz.Model.CheckInvariants;
 
 Dagaz.Model.CheckInvariants = function(board) {
   var design = Dagaz.Model.design;
-  for (var i in board.moves) {
-       var a = [];
-       var m = board.moves[i];
-       for (var j in m.actions) {
-           fp = m.actions[j][0];
-           tp = m.actions[j][1];
-           pc = m.actions[j][2];
-           if ((fp !== null) && (tp !== null) && (pc !== null)) {
-               var piece = board.getPiece(tp[0]);
-               if (pc[0].toString() == "Kauri") {
-                   if (!isKauri(piece)) {
-                       var dir = design.getDirection("win");
-                       if (dir !== null) {
-                           var dest = design.navigate(board.player, tp[0], dir);
-                           if (dest !== null) {
-                               a.push([ tp, [ dest ], null, 1]);
-                           }
+  _.each(board.moves, function(move) {
+      var fr  = 0;
+      var dir = move.mode;
+      if (move.isSimpleMove()) {
+          var pos = move.actions[0][0][0];
+          var piece = board.getPiece(pos);
+          var cnt = +piece.getValue(0);
+          var kcn = +piece.getValue(1);
+          if ((cnt === null) || (kcn === null) || (kcn == 0)) {
+              move.failed = true;
+              return;
+          }
+          var result = [0];
+          var kauri  = [0];
+          var ix = 1;
+          for (var i = 0; i < cnt + kcn; i++, ix++) {
+               pos = design.navigate(board.player, pos, dir);
+               if (pos === null) {
+                   move.failed = true;
+                   return;
+               }
+               if (ix >= 12) {
+                   ix = 0;
+               }
+               piece = board.getPiece(pos);
+               if (ix < result.length) {
+                   if (i < cnt) {
+                       result[ix]++;
+                   } else {
+                       kauri[ix]++;
+                   }
+               } else {
+                   if (piece === null) {
+                       if (i < cnt) {
+                           result.push(1);
+                           kauri.push(0);
+                       } else {
+                           result.push(0);
+                           kauri.push(1);
+                       }
+                   } else {
+                       if (i < cnt) {
+                           result.push(+piece.getValue(0) + 1);
+                           kauri.push(+piece.getValue(1));
+                       } else {
+                           result.push(+piece.getValue(0));
+                           kauri.push(+piece.getValue(1) + 1);
+                       }
+                   }
+               }
+          }
+          ix--;
+          // TODO: Capturing
+
+          var pos = move.actions[0][0][0];
+          for (var ix = 0; ix < result.length; ix++) {
+               var player = board.player;
+               if (!design.inZone(0, board.player, pos)) {
+                   player = design.nextPlayer(player);
+               }
+               var piece = Dagaz.Model.createPiece(0, player).setValue(0, result[ix]).setValue(1, kauri[ix]);
+               if (result[ix] + kauri[ix] == 0) {
+                   if (ix > 0) {
+                       move.capturePiece(pos);
+                       if (ix == 1) {
+                           move.actions[0][2] = [ Dagaz.Model.createPiece(1, board.player) ];
                        }
                    }
                } else {
-                   if (isKauri(piece)) {
-                       var dir = design.getDirection("loss");
-                       if (dir !== null) {
-                           var dest = design.navigate(board.player, tp[0], dir);
-                           if (dest !== null) {
-                               m.actions[j][1] = [ dest ];
+                   if (piece !== null) {
+                       if (ix == 1) {
+                           move.actions[0][2] = [ piece ];
+                       } else {
+                           if ((ix > 0) && (board.getPiece(pos) !== null)) {
+                               move.movePiece(pos, pos, piece);
+                           } else {
+                               move.dropPiece(pos, piece);
                            }
                        }
                    }
                }
-           }
-       }
-       if (a.length > 0) {
-           for (var j in m.actions) {
-               a.push(m[j]);
-           }
-           m.actions = a;
-       }
-  }
+               pos = design.navigate(board.player, pos, dir);
+          }
+          toReserve(design, board, board.player, move, fr);
+      }
+  });
   CheckInvariants(board);
 }
 
