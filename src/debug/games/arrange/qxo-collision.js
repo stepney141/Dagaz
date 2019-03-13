f0458b7f282368560b8e6509bf45ac1e9033c7f5
GlukKazan <glukkazan@gmail.com>
2019-03-13 16:36:02 +0300

    QXO (collision)

--- src/debug/games/arrange/qxo-collision.js
+++ src/debug/games/arrange/qxo-collision.js
@@ -8,6 +8,62 @@ Dagaz.Model.checkVersion = function(design, name, value) {
   }
 }
 
+var locatePair = function(design, board, piece, except) {
+  var r = null;
+  _.each(design.allPositions(), function(pos) {
+      if ((r === null) && (pos != except)) {
+          var target = board.getPiece(pos);
+          if ((target !== null) && (target.type == piece.type) && (target.player == piece.player)) {
+               r = pos;
+          }
+      }
+  });
+  return r;
+}
+
+var checkCollision = function(design, board, pos, collision) {
+  if (_.indexOf(collision, pos) >= 0) return true;
+  collision.push(pos);
+  var piece = board.getPiece(pos);
+  if (piece !== null) {
+      var pair = locatePair(design, board, piece, pos);
+      if (pair !== null) {
+          collision.push(pair);
+          pair = design.navigate(1, pair, 9);
+          if (pair !== null) {
+              var p = design.navigate(1, pair, 8);
+              while (p !== null) {
+                  var target = board.getPiece(p);
+                  if ((target !== null) && ((target.type != piece.type) || (target.player != piece.player))) {
+                      if (checkCollision(design, board, p, collision)) return true;
+                  }
+                  p = design.navigate(1, p, 8);
+              }
+          }
+          collision.pop();
+      }
+  }
+  collision.pop();
+  return false;
+}
+
+var getCollision = function(design, board) {
+  if (_.isUndefined(board.collision)) {
+      board.collision = [];
+      _.each(design.allPositions(), function(pos) {
+           if (board.collision.length == 0) {
+               var p = design.navigate(1, pos, 8);
+               while (p !== null) {
+                   var piece = board.getPiece(p);
+                   if ((piece !== null) && checkCollision(design, board, p, board.collision)) break;
+                   p = design.navigate(1, p, 8);
+               }
+           }
+      });
+  }
+  return board.collision;
+}
+
 var CheckInvariants = Dagaz.Model.CheckInvariants;
 
 Dagaz.Model.CheckInvariants = function(board) {
@@ -15,15 +71,30 @@ Dagaz.Model.CheckInvariants = function(board) {
   if (!_.isUndefined(board.move) && (board.move.mode == 4)) {
       board.moves = [];
   } else {
-      _.each(board.moves, function(move) {
-          if (move.isSimpleMove()) {
-              var pos = move.actions[0][0][0];
-              var piece = board.getPiece(pos);
-              if (piece !== null) {
-                  move.actions[0][2] = [piece.promote(+piece.type - 4)];
+      var collision = getCollision(design, board);
+      if (collision.length > 0) {
+          _.each(board.moves, function(move) {
+              if (move.isSimpleMove()) {
+                  var pos = move.actions[0][0][0];
+                  var piece = board.getPiece(pos);
+                  if (_.indexOf(collision, pos) < 0) {
+                      move.failed = true;
+                      return;
+                  }
+                  if (piece !== null) {
+                      move.actions[0][2] = [piece.promote(+piece.type - 4)];
+                  }
+              } else {
+                  move.failed = true;
               }
-          }
-      });
+          });
+      } else {
+          _.each(board.moves, function(move) {
+              if (move.isSimpleMove()) {
+                  move.failed = true;
+              }
+          });
+      }
   }
   CheckInvariants(board);
 }
