aa2c10c82622f8bddf84fdf5814bd817177dc0c0
GlukKazan <glukkazan@gmail.com>
2019-04-10 16:33:41 +0300

    Pyramid (prototype)

---
+++ src/debug/games/domino/pyramid-setup.js
@@ -0,0 +1,43 @@
+(function() {
+
+var checkVersion = Dagaz.Model.checkVersion;
+
+Dagaz.Model.checkVersion = function(design, name, value) {
+  if (name != "pyramid-setup") {
+     checkVersion(design, name, value);
+  }
+}
+
+var getSeed = function() {
+  var str = window.location.search.toString();
+  var result = str.match(/[?&]seed=([^&]*)/);
+  if (result) {
+      return result[1];
+  } else {
+      return "" + _.random(0, 10000);
+  }
+}
+
+Dagaz.Model.setup = function(board) {
+  var seed = getSeed();
+  console.log("Seed: " + seed);
+  Math.seedrandom(seed);
+  var design = Dagaz.Model.design;
+  var pieces = [];
+  for (var i = 0; i < 28; i++) {
+       pieces.push(i);
+  }
+  _.each(design.allPositions(), function(pos) {
+       if (!design.inZone(0, 1, pos)) return;
+       var ix = 0;
+       if (pieces.length > 1) {
+           ix = _.random(0, pieces.length - 1);
+       }
+       board.setPiece(pos, Dagaz.Model.createPiece(pieces[ix], 1));
+       if (pieces.length > 1) {
+           pieces = _.without(pieces, pieces[ix]);
+       }
+  });
+}
+
+})();
