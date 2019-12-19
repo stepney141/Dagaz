543b09ad41075f1afe168188a214fe1c23067564
GlukKazan <glukkazan@gmail.com>
2019-12-19 11:59:52 +0300

    Rook Chain (unstable)

--- src/debug/games/arrange/rook-chain-goal.js
+++ src/debug/games/arrange/rook-chain-goal.js
@@ -9,12 +9,13 @@ Dagaz.Model.checkVersion = function(design, name, value) {
 }
 
 var getGroups = function(design, board) {
-  var r = [];
+  if (_.isUndefined(board.groups)) {
+  board.groups = [];
   _.each(design.allPositions(), function(pos) {
      var piece = board.getPiece(pos);
      if (piece !== null) {
-         for (var i = 0; i < r.length; i++) {
-              if (_.indexOf(r[i].positions, pos) >= 0) return;
+         for (var i = 0; i < board.groups.length; i++) {
+              if (_.indexOf(board.groups[i].positions, pos) >= 0) return;
          }
          var x = {
               player: piece.player,
@@ -38,24 +39,85 @@ var getGroups = function(design, board) {
               if (cnt > 2) x.bads.push(x.positions[i]);
               if (cnt < 2) x.ends.push(x.positions[i]);
          }
-         r.push(x);
+         board.groups.push(x);
      }
   });
-  return r;
+  }
+  return board.groups;
 }
 
-var checkGroups = function(groups, player) {
-  var cnt = 0;
-  for (var i = 0; i < groups.length; i++) {
-       var x = groups[i];
-       if (x.player == player) {
-           if (x.bads.length > 0) return false;
-           if (x.ends.length < 1) return false;
-           if (x.ends.length > 2) return false;
-           cnt++;
-       }
+var backTrace = function(design, pos, level) {
+  while (_.isUndefined(level[pos]) || (level[pos] > 0)) {
+      var mn = null; var r = null;
+      _.each(design.allDirections(), function(dir) {
+           var p = design.navigate(1, pos, dir);
+           if ((p === null) || _.isUndefined(level[p])) return;
+           var l = level[p];
+           if (!_.isUndefined(level[pos]) && (l >= level[pos])) return;
+           if ((mn === null) || (l < mn)) {
+                mn = l;
+                r = p;
+           }
+      });
+      if (r === null) return null;
+      pos = r;
   }
-  return cnt == 1;
+  return pos;
+}
+
+var getEval = function(design, board, player) {
+  var groups = _.filter(getGroups(design, board), function(g) {
+      return (g.player == player) && (g.bads.length == 0) && (g.ends.length <= 2) && (g.ends.length > 0);
+  });
+  if (groups.length == 0) return 0;
+  var r = 0;
+  var node = groups.pop();
+  while (groups.length > 0) {
+      var g = []; var level = [];
+      _.each(node.ends, function(p) {
+          g.push(p);
+          level[p] = 0;
+      });
+      for (var i = 0; i < g.length; i++) {
+           var f = false;
+           _.each(design.allDirections(), function(dir) {
+                var p = design.navigate(player, g[i], dir);
+                if ((p === null) || (_.indexOf(g, p) < 0)) return;
+                var piece = board.getPiece(p);
+                if ((piece !== null) && (piece.player == player)) {
+                    var l = [];
+                    for (var j = 0; j < groups.length; j++) {
+                        if (!f) {
+                            var ix = _.indexOf(groups[j].ends, p);
+                            if (ix >= 0) {
+                                r += level[g[i]] + 1;
+                                if (node.ends.length > 1) {
+                                    var q = backTrace(design, p, level);
+                                    if (q !== null) {
+                                        node.ends = _.without(node.ends, q);
+                                    }
+                                }
+                                node.ends.push(p);
+                                f = true;
+                            }
+                        }
+                        l.push(groups[j]);
+                    }
+                    if (f) groups = l;
+                    return;
+                }
+                g.push(p);
+                level[p] = level[g[i]] + 1;
+           });
+           if (f) break;
+      }
+  }
+  return 1000 - r;
+}
+
+Dagaz.AI.eval = function(ai, design, board, player) {
+  return getEval(design, board, player) -
+         getEval(design, board, design.nextPlayer(player));
 }
 
 var getArity = function(design, board, player, pos) {
@@ -109,6 +171,20 @@ Dagaz.AI.heuristic = function(ai, design, board, move) {
       }
   });
   return r;
+}
+
+var checkGroups = function(groups, player) {
+  var cnt = 0;
+  for (var i = 0; i < groups.length; i++) {
+       var x = groups[i];
+       if (x.player == player) {
+           if (x.bads.length > 0) return false;
+           if (x.ends.length < 1) return false;
+           if (x.ends.length > 2) return false;
+           cnt++;
+       }
+  }
+  return cnt == 1;
 }
 
 var checkGoals = Dagaz.Model.checkGoals;
