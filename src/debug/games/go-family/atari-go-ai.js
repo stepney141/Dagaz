efb5b8177a0f5d991a198f698e4c173fcac8faa6
GlukKazan <glukkazan@gmail.com>
2020-01-17 13:17:55 +0300

    Atari Go (ab-ai-v5)

--- src/debug/games/go-family/atari-go-ai.js
+++ src/debug/games/go-family/atari-go-ai.js
@@ -14,11 +14,38 @@ var price =
     100, 150, 200, 200, 200, 200, 200, 150, 100,
       0, 100, 100, 100, 100, 100, 100, 100,   0 ];
 
+function Ai(parent) {
+  this.parent = parent;
+}
+
+var findBot = Dagaz.AI.findBot;
+
+Dagaz.AI.findBot = function(type, params, parent) {
+  if ((type == "fast") || (type == "opening")) {
+      return new Ai(parent);
+  } else {
+      return findBot(type, params, parent);
+  }
+}
+
 Dagaz.AI.getPrice = function(design, piece, pos) {
   return price[pos];
 }
 
 Dagaz.AI.see = function(design, board, move) {
+  if (move.actions.length > 0) {
+      var pos = move.actions[0][1][0];
+      for (var dir = 0; dir < design.dirs.length; dir++) {
+           var p = design.navigate(board.player, pos, dir);
+           if (p !== null) {
+               var piece = board.getPiece(p);
+               if (piece !== null) {
+                   var v = piece.getValue(0);
+                   if ((v !== null) && (v == 1)) return true;
+               }
+           }
+      }
+  }
   return false;
 }
 
@@ -32,8 +59,6 @@ var getEval = function(groups, player) {
       }
   });
   if (d === null) return 0;
-  if (d == 1) return -10000;
-  if (d == 2) return -100;
   return e + 1000 * d;
 }
 
@@ -43,7 +68,7 @@ Dagaz.AI.eval = function(design, params, board, player) {
       if (_.indexOf(done, pos) >= 0) return;
       var piece = board.getPiece(pos);
       if (piece === null) return;
-      var e = null; var d = 0; var c = 0;
+      var e = null; var d = []; var c = 0;
       var group = [ pos ];
       for (var i = 0; i < group.length; i++) {
           if ((e === null) || (e > price[group[i]])) {
@@ -55,7 +80,7 @@ Dagaz.AI.eval = function(design, params, board, player) {
                if (_.indexOf(done, p) >= 0) return;
                var x = board.getPiece(p);
                if (x === null) {
-                   d++;
+                   if (_.indexOf(d, p) < 0) d.push(p);
                    return;
                }
                if (x.player != piece.player) return;
@@ -66,13 +91,73 @@ Dagaz.AI.eval = function(design, params, board, player) {
       }
       groups.push({
           player: piece.player,
-          dame:   d,
+          dame:   d.length,
           price:  e,
           count:  c
       });
   });
   return getEval(groups, player) - 
          getEval(groups, design.nextPlayer(player));
+}
+
+var isAtari = function(design, board, pos, player) {
+  var r = false;
+  _.each(design.allDirections(), function(dir) {
+      var p = design.navigate(player, pos, dir);
+      if (p === null) return;
+      var piece = board.getPiece(p);
+      if (piece === null) return;
+      if (piece.player != player) return;
+      var v = piece.getValue(0);
+      if (v === null) return;
+      if (v == 1) r = true;
+  });
+  return r;
+}
+
+Ai.prototype.setContext = function(ctx, board) {
+  if (this.parent) {
+      this.parent.setContext(ctx, board);
+  }
+  ctx.board = board;
+  ctx.timestamp = Date.now();
+}
+
+Ai.prototype.getMove = function(ctx) {
+  ctx.board.moves = Dagaz.AI.generate(ctx, ctx.board);
+  if (ctx.board.moves.length > 0) {
+      for (var i = 0; i < ctx.board.moves.length; i++) {
+           var m = ctx.board.moves[i];
+           if (m.actions.length > 0) {
+               var pos = m.actions[0][1][0];
+               if (isAtari(ctx.design, ctx.board, pos, ctx.design.nextPlayer(ctx.board.player))) {
+                   return {
+                      done: true,
+                      move: m,
+                      time: Date.now() - ctx.timestamp,
+                      ai:  "fast"
+                   };
+               }
+           }
+      }
+      for (var i = 0; i < ctx.board.moves.length; i++) {
+           var m = ctx.board.moves[i];
+           if (m.actions.length > 0) {
+               var pos = m.actions[0][1][0];
+               if (isAtari(ctx.design, ctx.board, pos, ctx.board.player)) {
+                   return {
+                      done: true,
+                      move: m,
+                      time: Date.now() - ctx.timestamp,
+                      ai:  "fast"
+                   };
+               }
+           }
+      }
+  }
+  if (this.parent) {
+      return this.parent.getMove(ctx);
+  }
 }
 
 })();
