21590b6736329b54c3a043f811589a8f959c08d2
GlukKazan <glukkazan@gmail.com>
2020-03-31 09:25:20 +0300

    Ordo (workaround)

--- src/debug/games/breakthrough/ordo-extension.js
+++ src/debug/games/breakthrough/ordo-extension.js
@@ -122,7 +122,7 @@ Dagaz.Model.CheckInvariants = function(board) {
   var f = false;
   _.each(board.moves, function(move) {
       if (f) return;
-      if (move.mode == 1) {
+      if (move.mode == board.player) {
           f = true;
       }
   });
@@ -131,7 +131,7 @@ Dagaz.Model.CheckInvariants = function(board) {
   }
   _.each(board.moves, function(move) {
       var b = board.apply(move);
-      if (f && (move.mode == 1)) {
+      if (f && (move.mode == board.player)) {
           move.failed = true;
           return;
       }
