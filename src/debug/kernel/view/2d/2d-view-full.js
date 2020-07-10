eca576138505c5c6dcb6476f6fa634c6fd943b5f
GlukKazan <glukkazan@gmail.com>
2020-07-10 14:19:40 +0300

    2d-view-full (animate)

--- src/debug/kernel/view/2d/2d-view-full.js
+++ src/debug/kernel/view/2d/2d-view-full.js
@@ -6,15 +6,23 @@ var MessageCode = {
   MOUSE_PKM:            2,
   MOUSE_WHEEL:          3,
   KEY_PRESSED:          4,
-  TURN_MOVE:            5
+  TURN_MOVE:            5,
+  MARK_CAPTURES:        6,
+  MARK_TARGETS:         7
 };
 
-var isValid           = false;
-var mouseX            = 0;
-var mouseY            = 0;
-var mousePressed      = false; // TODO
+var isValid             = false;
+var mouseX              = 0;
+var mouseY              = 0;
+var mousePressed        = false; // TODO
 
-function Region(x, y, dx, dy, isActive) {
+Dagaz.View.SHIFT_X      = 0;
+Dagaz.View.SHIFT_Y      = 0;
+Dagaz.View.STRIKE_ALPHA = 0.5;
+Dagaz.View.STEP_CNT     = 3;
+
+function Region(x, y, dx, dy, isActive, extension) {
+  this.e = extension;
   this.x = x; this.dx = dx;
   this.y = y; this.dy = dy;
   this.isActive = isActive;
@@ -23,19 +31,21 @@ function Region(x, y, dx, dy, isActive) {
   this.positions = [];
 }
 
-Region.prototype.addRegion = function(x, y, dx, dy, isActive) {
-  var r = new Region(x, y, dx, dy, isActive);
+Region.prototype.addRegion = function(x, y, dx, dy, isActive, extension) {
+  var r = new Region(x, y, dx, dy, isActive, extension);
   r.view = this.view;
   r.parent = this;
   r.zValue = this.regions.length;
   this.regions.push(r);
+  return r;
 }
 
-Region.prototype.addBoard = function(name, turns, selector) {
+Region.prototype.addBoard = function(name, turns, selector, extension) {
   if (!_.isUndefined(selector) && (selector != Dagaz.Model.getResourceSelector())) return;
   var board = {
       h: document.getElementById(name),
-      t: turns
+      t: turns,
+      e: extension
   };
   this.boards.push(board);
 }
@@ -51,6 +61,17 @@ Region.prototype.addPosition = function(name, x, y, dx, dy) {
   this.positions.push(pos);
 }
 
+Region.prototype.findPosition = function(name) {
+  for (var i = 0; i < this.positions.length; i++) {
+       if (this.positions[i].name == name) return this.positions[i];
+  }
+  for (var i = 0; i < this.regions.length; i++) {
+       var r = this.regions[i].findPosition(name);
+       if (r !== null) return r;
+  }
+  return null;
+}
+
 Region.prototype.isLoaded = function() {
   for (var i = 0; i < this.regions.length; i++) {
        if (!this.regions[i].isLoaded()) return false;
@@ -58,23 +79,68 @@ Region.prototype.isLoaded = function() {
   for (var i = 0; i < this.boards.length; i++) {
        var image = this.boards[i].h;
        if (!image.complete || (image.naturalWidth == 0)) return false;
+       this.boards[i].dx = image.naturalWidth;
+       this.boards[i].dy = image.naturalHeight;
   }
   return true;
 }
 
-Region.prototype.draw = function(x, y) {
+var drawMark = function(ctx, pos, x, y, color) {
+  x += pos.x; y += pos.y;
+  x += (pos.dx / 2) | 0;
+  y += (pos.dy / 2) | 0;
+  var r = pos.dx / 4;
+  if (Math.abs(pos.dy - pos.dx) > 10) {
+      r = Math.min(pos.dy, pos.dx) / 2;
+  }
+  if (!_.isUndefined(Dagaz.View.MARK_R)) {
+      r = Dagaz.View.MARK_R;
+  }
+  ctx.beginPath();
+  ctx.fillStyle = color;
+  ctx.arc(x + Dagaz.View.SHIFT_X, y + Dagaz.View.SHIFT_Y, r, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.stroke();
+}
+
+Region.prototype.draw = function(ctx, x, y) {
   if (!this.isActive) return;
   _.each(r.boards, function(board) {
       if (!_.isUndefined(board.t) && !_.isUndefined(this.turn)) {
           if (_.indexOf(board.t, +this.turn) < 0) return;
       }
       ctx.drawImage(board.h, x, y);
+      if (!_.isUndefined(board.e)) {
+          board.e(view, ctx, board);
+      }
   });
-  // TODO: Show pieces, marks, etc.
+  for (var i = 0; i < this.positions.length; i++) {
+       if (!_.isUndefined(this.positions[i].setup)) {
+           var setup = this.positions[i].setup;
+           if (setup.c > 0) {
+               setup.x += setup.dx;
+               setup.y += setup.dy;
+               setup.c--;
+           }
+           setup.piece.e(view, ctx, this.positions[i], x + setup.x, y + setup.y);
+       }
+  }
+  if (!_.isUndefined(this.markTargets)) {
+       for (var i = 0; i < this.positions.length; i++) {
+            var ix = Dagaz.Model.stringToPos(this.positions[i].name, this.view.design);
+            if (_.indexOf(this.markTargets, ix) >= 0) {
+                drawMark(ctx, this.positions[i], x, y, "#00AA00");
+            }
+       }
+  }
+  // TODO: Show drops, etc.
 
+  if (!_.isUndefined(this.e)) {
+       this.e(view, ctx, this);
+  }
   for (var i = 0; i < this.regions.length; i++) {
        var r = this.regions[i];
-       r.draw(x + r.x, y + r.y);
+       r.draw(ctx, x + r.x, y + r.y);
   }
 }
 
@@ -137,8 +203,9 @@ Region.prototype.send = function(code, event, x, y) {
   return r;
 }
 
-function View(dx, dy) {
-  this.root = new Region(0, 0, dx, dy, true);
+function View(design) {
+  this.design = design;
+  this.root = new Region(0, 0, canvas.width, canvas.height, true);
   this.root.view = this;
   this.pieces = [];
 }
@@ -150,13 +217,39 @@ Dagaz.View.getView = function() {
   return Dagaz.View.view;
 }
 
-View.prototype.addPiece = function(name) {
+var drawPiece = function(view, ctx, pos, x, y) {
+  var isSaved = false;
+  var piece = pos.setup.piece;
+  if (Dagaz.Model.showCaptures) {
+      var ix = Dagaz.Model.stringToPos(pos.name, view.design);
+      if (!_.isUndefined(this.markCaptures) && (_.indexOf(this.markCaptures, ix) >= 0)) {
+          ctx.save();
+          ctx.globalAlpha = Dagaz.View.STRIKE_ALPHA;
+          isSaved = true;
+      }
+  }
+  x += (pos.dx - piece.dx) / 2 | 0;
+  y += (pos.dy - piece.dy) / 2 | 0;
+  ctx.drawImage(piece.h, x, y, piece.dx, piece.dy);
+  if (isSaved) {
+      ctx.restore();
+  }
+}
+
+View.prototype.addPiece = function(name, extension) {
   var piece = {
+      n: name,
       h: document.getElementById(name),
-      dx: 0,
-      dy: 0
+      e: _.isUndefined(extension) ? drawPiece : extension
   };
   this.pieces.push(piece);
+}
+
+View.prototype.findPiece = function(name) {
+  for (var i = 0; i < this.pieces.length; i++) {
+       if (this.pieces[i].n == name) return this.pieces[i];
+  }
+  return null;
 }
 
 View.prototype.isLoaded = function() {
@@ -173,15 +266,121 @@ View.prototype.isLoaded = function() {
   return this.allLoaded;
 }
 
+View.prototype.send = function(code, event, x, y) {
+  if (code == MARK_CAPTURES) {
+      this.markCaptures = event;
+      return true;
+  }
+  if (code == MARK_TARGETS) {
+      this.markTargets = event;
+      return true;
+  }
+
+  return this.root.send(code, event, x, y);
+}
+
 View.prototype.invalidate = function() {
    isValid = false;
 }
 
-View.prototype.draw = function() {
-  if (!this.isLoaded() && !isValid) return;
-  this.root.draw();
-  // TODO: Show moving pieces
+View.prototype.addSetup = function(name, piece) {
+  var pos = this.root.findPosition(name);
+  if (pos !== null) {
+      pos.setup = {
+          piece: piece,
+          x: pos.x,
+          y: pos.y,
+          dx: 0,
+          dy: 0,
+          c: 0
+      };
+      this.invalidate();
+  }
+}
 
+View.prototype.apply = function(move) {
+  if (!_.isUndefined(this.move)) return false;
+  this.move = move;
+  this.step = 0;
+  this.changes = [];
+  return true;
+}
+
+View.prototype.animate = function() {
+  if (!_.isUndefined(this.changes) && !_.isUndefined(this.move)) {
+       var f = true;
+       for (var i = 0; i < this.changes.length; i++) {
+           if (this.changes[i].c > 0) {
+               this.changes[i].c--;
+               f = false;
+           }
+       }
+       if (f) {
+           for (var i = 0; i < this.changes.length; i++) {
+                var c = this.changes[i];
+                if ((c.n !== null) && (c.o !== null) && (c.s !== null)) {
+                    c.s.x = c.n.x; c.s.dx = 0;
+                    c.s.y = c.n.y; c.s.dy = 0;
+                    c.n.setup = c.s; c.s.c = 0;
+                    delete c.o;
+                }
+           }
+           for (var i = 0; i < this.changes.length; i++) {
+                var c = this.changes[i];
+                if ((c.o === null) && (c.n !== null) && (c.s !== null)) {
+                    c.s.x = c.n.x; c.s.dx = 0;
+                    c.s.y = c.n.y; c.s.dy = 0;
+                    c.n.setup = c.s; c.s.c = 0;
+                }
+           }
+           for (var i = 0; i < this.changes.length; i++) {
+                var c = this.changes[i];
+                if ((c.o !== null) && (c.n === null)) {
+                    delete c.o;
+                }
+           }
+           this.step++;
+           this.changes = [];
+           for (var i = 0; i < this.move.actions.length; i++) {
+                var a = this.move.actions[i];
+                var o = null; var n = null;
+                var s = null; var c = 0;
+                if (a[0] !== null) o = this.root.findPosition(Dagaz.Model.posToString(a[0][0], this.design));
+                if (a[1] !== null) n = this.root.findPosition(Dagaz.Model.posToString(a[1][0], this.design));
+                if (a[2] !== null) {
+                    s = {
+                       piece: this.findPiece(a[2][0].getOwner() + a[2][0].getType());
+                    };
+                }
+                if ((o !== null) && (n !== null)) {
+                   c = Dagaz.View.STEP_CNT;
+                   if (s === null) {
+                       s = o.setup;
+                   }
+                   o.setup.dx = ((n.x - o.x) / c) | 0;
+                   o.setup.dy = ((n.y - o.y) / c) | 0;
+                   o.setup.c = c;
+                }
+                this.changes.push({
+                   c: c, o: o, n: n, s: s
+                });
+           }          
+           if (this.changes.length == 0) {
+               delete this.changes;
+               delete this.move;
+           }
+       }
+       this.invalidate();
+  }
+}
+
+View.prototype.draw = function() {
+  if (!this.isLoaded()) return;
+  this.animate();
+  if (!isValid) return;
+  var ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, this.root.dx, this.root.dy);
+  this.root.draw(ctx, 0, 0);
   isValid = true;
 }
 
