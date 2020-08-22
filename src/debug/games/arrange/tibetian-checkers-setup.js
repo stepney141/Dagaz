2ecaa6fe82bd72a344597ba763f70d7ccb5bacb4
GlukKazan <glukkazan@gmail.com>
2020-08-22 11:11:52 +0300

    Tibetian Checkers (setup fixed)

--- src/debug/games/arrange/tibetian-checkers-setup.js
+++ src/debug/games/arrange/tibetian-checkers-setup.js
@@ -8,13 +8,51 @@ Dagaz.Model.checkVersion = function(design, name, value) {
   }
 }
 
-var getSetup = function() {
+var getName = function() {
+  var str = window.location.pathname.toString();
+  var result = str.match(/\/([^.\/]+)\./);
+  if (result) {
+      return result[1].replace("-board", "").replace("-ai", "").replace("-kanji", "");
+  } else {
+      return str;
+  }
+}
+
+var badName = function(str) {
+  var result = str.match(/[?&]game=([^&*]*)/);
+  if (result) {
+      return result[1] != getName();
+  } else {
+      return true;
+  }
+}
+
+var getCookie = function() {
+  var result = localStorage.getItem('dagaz.setup');
+  if (result) {
+      if (badName(result)) return "";
+      return "?setup=" + result;
+  } else {
+      return "";
+  }
+}
+
+var getSetup = function(setup) {
   var str = window.location.search.toString();
+  if (setup) {
+      str = setup;
+  }
   var result = str.match(/[?&]setup=([^&]*)/);
   if (result) {
       return result[1];
   } else {
-      return "";
+      str = getCookie();
+      result = str.match(/[?&]setup=([^&]*)/);
+      if (result) {
+          return result[1];
+      } else {
+          return "";
+      }
   }
 }
 
