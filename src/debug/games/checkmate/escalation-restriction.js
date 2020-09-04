638eeb66dc1ec7bd798546834f917f54ba2fff13
GlukKazan <glukkazan@gmail.com>
2020-09-04 15:36:57 +0300

    Escalation (completed)

--- src/debug/games/checkmate/escalation-restriction.js
+++ src/debug/games/checkmate/escalation-restriction.js
@@ -12,19 +12,21 @@ var CheckInvariants = Dagaz.Model.CheckInvariants;
 
 Dagaz.Model.CheckInvariants = function(board) {
   var design = Dagaz.Model.design;
+  var moves = [];
   _.each(board.moves, function(move) {
-      if (move.mode == 0) return;
       if (!move.isSimpleMove()) return;
-      var pos = move.actions[0][0][0];
-      var piece = board.getPiece(pos);
-      if (piece === null) return;
-      var v = piece.getValue(0);
-      if (v === null) v = 0;
-      var mask = 1 << (move.mode - 1);
-      if ((mask & v) == 0) {
-          move.failed = true;
+      if (move.mode != 0) {
+          var pos = move.actions[0][0][0];
+          var piece = board.getPiece(pos);
+          if (piece === null) return;
+          var v = piece.getValue(0);
+          if (v === null) v = 0;
+          var mask = 1 << (move.mode - 1);
+          if ((mask & v) == 0) return;
       }
+      moves.push(move);
   });
+  board.moves = moves;
   CheckInvariants(board);
 }
 
