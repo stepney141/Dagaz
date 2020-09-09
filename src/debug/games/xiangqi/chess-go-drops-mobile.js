ab074a9b3674b6d394770fcdde8c98179848d392
GlukKazan <glukkazan@gmail.com>
2020-09-09 16:00:09 +0300

    Chess Go (mobile)

--- src/debug/games/xiangqi/chess-go-drops-mobile.js
+++ src/debug/games/xiangqi/chess-go-drops-mobile.js
@@ -12,6 +12,32 @@ Dagaz.Model.checkVersion = function(design, name, value) {
   }
 }
 
+var noMoves = function(board, pos) {
+  if (pos < Dagaz.Model.WIDTH * Dagaz.Model.WIDTH) return false;
+  if (!board) return true;
+  var r = true;
+  _.each(board.moves, function(move) {
+      if (move.actions.length == 0) return;
+      if (move.actions[0][0] === null) return;
+      if (pos == move.actions[0][0][0]) r = false;
+  });
+  return r;
+}
+
+Dagaz.View.showPiece = function(view, ctx, frame, pos, piece, model, x, y) {
+  var f = false;
+  var board = Dagaz.Controller.app.board;
+  if (noMoves(board, pos)) {
+      ctx.save();
+      ctx.globalAlpha = 0.3;
+      f = true;
+  }
+  ctx.drawImage(piece.h, x, y, piece.dx, piece.dy);
+  if (f) {
+      ctx.restore();
+  }
+}
+
 var CheckInvariants = Dagaz.Model.CheckInvariants;
 
 Dagaz.Model.CheckInvariants = function(board) {
@@ -32,6 +58,7 @@ Dagaz.Model.CheckInvariants = function(board) {
                 if (f || (piece.type == 7)) {
                     _.each(positions, function(p) {
                          var move = Dagaz.Model.createMove(0);
+                         if (piece.type == 7) move.mode = 1;
                          move.movePiece(pos, p, piece);
                          if (once && (_.indexOf([4, 5], +piece.type) >= 0)) {
                              for (var q = size; q < design.positions.length; q++) {
@@ -48,6 +75,17 @@ Dagaz.Model.CheckInvariants = function(board) {
                 }
            }
       }
+  }
+  var moves = [];
+  if (!_.isUndefined(board.move) && (board.move.mode == 1)) {
+      _.each(board.moves, function(move) {
+           if (move.mode == 1) {
+               moves.push(move);
+           }
+      });
+  }
+  if (moves.length > 0) {
+      board.moves = moves;
   }
   CheckInvariants(board);
 }
--- src/debug/games/xiangqi/chess-go-mobile.js
+++ src/debug/games/xiangqi/chess-go-mobile.js
@@ -262,50 +262,50 @@ Dagaz.Model.BuildDesign = function(design) {
     design.addDrop(0, 0, [], 0);
 
     design.addPiece("Soldier", 1);
-    design.addMove(1, 1, [7], 0);
-    design.addMove(1, 1, [3], 0);
-    design.addMove(1, 1, [4], 0);
-    design.addMove(1, 1, [1], 0);
+    design.addMove(1, 1, [7], 2);
+    design.addMove(1, 1, [3], 2);
+    design.addMove(1, 1, [4], 2);
+    design.addMove(1, 1, [1], 2);
 
     design.addPiece("Horse", 2);
-    design.addMove(2, 2, [7, 6], 0);
-    design.addMove(2, 2, [7, 5], 0);
-    design.addMove(2, 2, [1, 2], 0);
-    design.addMove(2, 2, [1, 0], 0);
-    design.addMove(2, 2, [4, 6], 0);
-    design.addMove(2, 2, [3, 5], 0);
-    design.addMove(2, 2, [4, 2], 0);
-    design.addMove(2, 2, [3, 0], 0);
+    design.addMove(2, 2, [7, 6], 2);
+    design.addMove(2, 2, [7, 5], 2);
+    design.addMove(2, 2, [1, 2], 2);
+    design.addMove(2, 2, [1, 0], 2);
+    design.addMove(2, 2, [4, 6], 2);
+    design.addMove(2, 2, [3, 5], 2);
+    design.addMove(2, 2, [4, 2], 2);
+    design.addMove(2, 2, [3, 0], 2);
 
     design.addPiece("Elephant", 3);
-    design.addMove(3, 2, [6, 6], 0);
-    design.addMove(3, 2, [5, 5], 0);
-    design.addMove(3, 2, [2, 2], 0);
-    design.addMove(3, 2, [0, 0], 0);
+    design.addMove(3, 2, [6, 6], 2);
+    design.addMove(3, 2, [5, 5], 2);
+    design.addMove(3, 2, [2, 2], 2);
+    design.addMove(3, 2, [0, 0], 2);
 
     design.addPiece("Chariot", 4);
-    design.addMove(4, 3, [7, 7], 0);
-    design.addMove(4, 3, [3, 3], 0);
-    design.addMove(4, 3, [4, 4], 0);
-    design.addMove(4, 3, [1, 1], 0);
+    design.addMove(4, 3, [7, 7], 2);
+    design.addMove(4, 3, [3, 3], 2);
+    design.addMove(4, 3, [4, 4], 2);
+    design.addMove(4, 3, [1, 1], 2);
 
     design.addPiece("Cannon", 5);
-    design.addMove(5, 4, [7, 7, 7, 7], 0);
-    design.addMove(5, 4, [3, 3, 3, 3], 0);
-    design.addMove(5, 4, [4, 4, 4, 4], 0);
-    design.addMove(5, 4, [1, 1, 1, 1], 0);
+    design.addMove(5, 4, [7, 7, 7, 7], 2);
+    design.addMove(5, 4, [3, 3, 3, 3], 2);
+    design.addMove(5, 4, [4, 4, 4, 4], 2);
+    design.addMove(5, 4, [1, 1, 1, 1], 2);
 
     design.addPiece("Mandarin", 6);
-    design.addMove(6, 5, [6], 0);
-    design.addMove(6, 5, [5], 0);
-    design.addMove(6, 5, [2], 0);
-    design.addMove(6, 5, [0], 0);
+    design.addMove(6, 5, [6], 2);
+    design.addMove(6, 5, [5], 2);
+    design.addMove(6, 5, [2], 2);
+    design.addMove(6, 5, [0], 2);
 
     design.addPiece("General", 7);
-    design.addMove(7, 6, [7], 0);
-    design.addMove(7, 6, [3], 0);
-    design.addMove(7, 6, [4], 0);
-    design.addMove(7, 6, [1], 0);
+    design.addMove(7, 6, [7], 2);
+    design.addMove(7, 6, [3], 2);
+    design.addMove(7, 6, [4], 2);
+    design.addMove(7, 6, [1], 2);
 
     design.setup("White", "Soldier", 87);
     design.setup("White", "Soldier", 89);
