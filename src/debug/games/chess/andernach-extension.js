72d6bb60c5b1ac3b067173e2900a4f879130fbd9
GlukKazan <glukkazan@gmail.com>
2021-02-05 17:35:05 +0300

    Andernach Chess (fix)

--- src/debug/games/chess/andernach-extension.js
+++ src/debug/games/chess/andernach-extension.js
@@ -14,8 +14,12 @@ Dagaz.Model.CheckInvariants = function(board) {
   var design = Dagaz.Model.design;
   _.each(board.moves, function(move) {
       if (!move.isSimpleMove()) return;
-      var pos = move.actions[0][1][0];
+      var pos = move.actions[0][0][0];
       var piece = board.getPiece(pos);
+      if (piece === null) return;
+      if (piece.type == 5) return;
+      pos = move.actions[0][1][0];
+      piece = board.getPiece(pos);
       if (piece === null) return;
       var pieces = [];
       _.each(move.actions[0][2], function(p) {
